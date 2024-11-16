3c6775a3eba82dff139ac2974213b2b77b353425
kranthi1434 <kranthijana1997@gmail.com>
2024-11-16 12:17:34 +0530

    article more commits

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,6 +4,9 @@ const Article = () =>{
   return(
     <>
       <p>This is Article component</p>
+      <p>This is Article component 2</p>
+      <p>This is Article component 3</p>
+      <p>This is Article component 4</p>
     </>
   )
 }
